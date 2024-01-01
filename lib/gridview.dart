3311ec72f09ebc3944509bb12ce015c79b0bda16
shamim2810 <shamim149120@gmail.com>
2024-01-01 16:06:12 +0600

    ThemeData_Cupertino

--- lib/gridview.dart
+++ lib/gridview.dart
@@ -4,94 +4,56 @@ class HomePage extends StatelessWidget {
   HomePage({super.key});
 
 
-  mySnackBar(context, message){
-    return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
-  }
-
-  var MyItems = [
-    {
-      "img": "https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_640.jpg",
-      "title": "Basic Home Laptop",
-      "price": "499.99"
-    },
-    {
-      "img": "https://cdn.pixabay.com/photo/2015/05/04/10/16/vegetables-752153_640.jpg",
-      "title": "Student Workstation",
-      "price": "579.99"
-    },
-    {
-      "img": "https://cdn.pixabay.com/photo/2016/08/11/08/04/vegetables-1584999_640.jpg",
-      "title": "Gaming Powerhouse",
-      "price": "1299.99"
-    },
-    {
-      "img": "https://cdn.pixabay.com/photo/2013/07/13/01/22/vegetables-155616_640.png",
-      "title": "Professional Desktop",
-      "price": "825.50"
-    },
-    {
-      "img": "https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_640.jpg",
-      "title": "Ultra-Thin Laptop",
-      "price": "999.00"
-    },
-    {
-      "img": "https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_640.jpg",
-      "title": "Budget Desktop PC",
-      "price": "399.99"
-    },
-    {
-      "img": "https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_640.jpg",
-      "title": "Compact Netbook",
-      "price": "299.99"
-    },
-    {
-      "img": "https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_640.jpg",
-      "title": "High-End Workstation",
-      "price": "1450.00"
-    },
-    {
-      "img": "https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_640.jpg",
-      "title": "Multimedia Entertainment System",
-      "price": "850.00"
-    },
-    {
-      "img": "https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_640.jpg",
-      "title": "Portable Business Laptop",
-      "price": "650.00"
-    }
-  ];
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Grid view Builder'),
-        backgroundColor: Colors.grey,
-        centerTitle: true,
+        title: Text('Theme Data'),
+        //backgroundColor: Colors.grey,
+        //centerTitle: true,
       ),
       body: SafeArea(
-        child: GridView.builder(
-          scrollDirection: Axis.vertical,
-            itemCount: MyItems.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, crossAxisSpacing: 2),
-            itemBuilder: (context, index){
-              return InkWell(
-                onTap: (){mySnackBar(context, MyItems[index]['price'].toString());},
-                child: Card(
-                  elevation: 2.0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(child: Image.network(MyItems[index]['img'].toString(),fit: BoxFit.fill,)),
-                      Padding(padding: EdgeInsets.all(8.0),child: Text(MyItems[index]['title'].toString()),),
-                      Padding(padding: EdgeInsets.all(8.0),child: Text(MyItems[index]['price'].toString()),)
-                    ],
-                  ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
                 ),
-              );
-            })
-
+                backgroundColor: Colors.deepOrangeAccent,
+              ),
+                onPressed: (){},
+                child: Text('Tep here'),
+            ),
+            SizedBox(height: 5,),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                backgroundColor: Colors.cyan,
+              ),
+              onPressed: (){},
+              child: Text('Tep here'),
+            ),
+            SizedBox(height: 5,),
+            ElevatedButton(
+              onPressed: (){},
+              child: Text('Tep here'),
+            ),
+            SizedBox(height: 5,),
+            ElevatedButton(
+              onPressed: (){},
+              child: Text('Tep here'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+        child: Icon(Icons.add),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,65 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-      //home: HomeActivity(),
+    return CupertinoApp(
+      home: CupertinoHomePage(),
+    );
+
+    // return MaterialApp(
+    //   home: HomePage(),
+    //   theme: ThemeData(
+    //     primarySwatch: Colors.grey,
+    //     brightness: Brightness.light,
+    //     elevatedButtonTheme: ElevatedButtonThemeData(
+    //       style: ElevatedButton.styleFrom(
+    //         shape: RoundedRectangleBorder(
+    //           borderRadius: BorderRadius.circular(0),
+    //         ),
+    //         backgroundColor: Colors.deepOrangeAccent,
+    //       ),
+    //     ),
+    //     scaffoldBackgroundColor: Colors.blueGrey,
+    //     appBarTheme: AppBarTheme(
+    //       backgroundColor: Colors.red,
+    //       titleTextStyle: TextStyle(
+    //         fontWeight: FontWeight.bold,
+    //         fontSize: 25,
+    //         color: Colors.white,
+    //       ),
+    //       centerTitle: true,
+    //     ),
+    //     floatingActionButtonTheme: FloatingActionButtonThemeData(
+    //       backgroundColor: Colors.brown,
+    //       elevation: 2.0,
+    //       iconSize: 25,
+    //     )
+    //   ),
+    //   darkTheme: ThemeData(
+    //     brightness: Brightness.dark,
+    //   ),
+    //   themeMode: ThemeMode.light,
+    // );
+  }
+}
+class CupertinoHomePage extends StatelessWidget {
+  const CupertinoHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Home'),
+          backgroundColor: Colors.grey,
+          leading: Icon(CupertinoIcons.home),
+        ),
+        child: Column(
+          children: [
+            CupertinoButton(child: Text('Click'), onPressed: (){}),
+            CupertinoButton.filled(child: Text('Click'), onPressed: (){}),
+            SizedBox(height: 5,),
+            ElevatedButton(onPressed: (){}, child: Text('Tep here')),
+          ],
+        )
     );
   }
 }
