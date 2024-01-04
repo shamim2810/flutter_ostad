331627e594ecf2ecbdc4684ccdc71e6eaec0fb75
shamim2810 <shamim149120@gmail.com>
2024-01-04 14:56:00 +0600

    Stateful_Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ostad/homescreen.dart';
+
+/// KISS - Keep It Simple, Stupid
 
 void main(){
   runApp(MyApp());
@@ -11,7 +11,112 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
+
+/// Mutable
+/// Immutable
+
+/// Widget - two types
+/// Stateless - Immutable
+/// Stateful - Mutable
+
+/// Stateful - two component
+/// Widget
+/// State
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+            },
+            child: Text('Go to Home')),
+      ),
+    );
+  }
+}
+
+
+class HomeScreen extends StatefulWidget {
+   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    print('init State');
+    /// on start a screen
+    ///  1
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('didChangeDependencies');
+    /// 2
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    /// 4
+    print('onUpdateWidget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    /// 3
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Center(
+        child: Text(count.toString(), style: TextStyle(
+          fontSize: 32,
+        ),),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            count = count + 1;
+            setState(() {
+
+            });
+          },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+  @override
+  void deactivate() {
+    super.deactivate();
+    print('deactivated');
+  }
+
+  @override
+  void dispose() {
+    print('dispose');
+    super.dispose();
+  }
+}
+
