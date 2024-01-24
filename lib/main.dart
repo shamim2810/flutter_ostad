6b3c7d1f0fc240090bd98e6d9b6eb9f4a6314b47
shamim2810 <shamim149120@gmail.com>
2024-01-24 18:38:54 +0600

    water_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ostad/sum_app.dart';
+import 'package:flutter_ostad/water_app.dart';
 
 void main(){
   runApp(MyApp());
